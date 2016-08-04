c1c71268a8feddb12ab2b77f1bc2dbba27bd56a6
Jose Miguel Bejarano <yo@josemiguel.org>
2016-08-04 16:04:10 +0200

    changed input

--- src/WodList.js
+++ src/WodList.js
@@ -1,32 +1,42 @@
 import React from 'react';
-
 import './App.css';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {List, ListItem} from 'material-ui/List';
 import RaisedButton from 'material-ui/RaisedButton';
-// import ContentInbox from 'material-ui/svg-icons/content/inbox';
-// import ActionGrade from 'material-ui/svg-icons/action/grade';
-// import ContentSend from 'material-ui/svg-icons/content/send';
-import ContentDrafts from 'material-ui/svg-icons/content/drafts';
-// import ActionInfo from 'material-ui/svg-icons/action/info';
-
 import TextField from 'material-ui/TextField';
+import ContentDrafts from 'material-ui/svg-icons/content/drafts';
 
 class WodList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			form: {
+				text: ''
+			}
+		};
+	}
+
   _handleIcon(icon) {
     return <ContentDrafts />
   }
 
+	_handleInputChange(e) {
+		this.setState({
+			form: {
+				text: e.target.value
+			}
+		});
+	}
+
 	_handleSubmit(event) {
 		event.preventDefault();
-		let wodName = this._wodName;
 
-		if (wodName.input.value !== null && wodName.input.value !== undefined && wodName.input.value !== '' ) {
-			// console.log(wodName.input.value);
-			this.props.addWod(wodName.input.value);
+		let text = this.state.form.text;
+
+		if (text !== null && text !== undefined && text !== '' ) {
+			this.props.addWod(text);
 		}
-
 	}
 
 	render() {
@@ -38,9 +48,10 @@ class WodList extends React.Component {
 					<List>
 						<form onSubmit={this._handleSubmit.bind(this)}>
 							<TextField
-								ref={(input) => this._wodName = input}
 								fullWidth={true}
 								hintText="Write a WOD"
+								value={this.state.form.text}
+								onChange={this._handleInputChange.bind(this)}
 							/>
 							<RaisedButton label="Submit" primary={true} onClick={this._handleSubmit.bind(this)} />
 						</form>
