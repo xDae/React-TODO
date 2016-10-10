ef295f82735f3d1bb5b8a073bcb27b7565b7afa3
Jose Miguel Bejarano <yo@josemiguel.org>
2016-10-10 16:17:32 +0200

    update CRA

--- src/Components/WodListContainer.js
+++ src/Components/WodListContainer.js
@@ -12,15 +12,15 @@ import Wodlist from './WodList';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class WodListContainer extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      list: []
-    };
+	constructor() {
+		super();
+		this.state = {
+			list: []
+		};
 
 		this._adddWod = this._adddWod.bind(this)
 		this._deleteWod = this._deleteWod.bind(this);
-  }
+	}
 
 	componentWillMount() {
 		localforage.getItem('wodList')
@@ -33,17 +33,17 @@ class WodListContainer extends React.Component {
 		})
 	}
 
-	updateState = (wod) => {
+	updateState = wod => {
 		this.setState({
 			list: wod
 		});
 	};
 
-  _adddWod(name) {
-    let wod = {
-      id: uniqueId(),
-      name
-    };
+	_adddWod(name) {
+		let wod = {
+			id: uniqueId(),
+			name
+		};
 
 		let newState = this.state.list.concat([wod]);
 
@@ -52,11 +52,11 @@ class WodListContainer extends React.Component {
 			this.updateState(newState);
 		})
 		.catch(function (err) {
-		  console.error(err);
+			console.error(err);
 		});
-  }
+	}
 
-  _deleteWod(id) {
+	_deleteWod(id) {
 		let newState = this.state.list.filter(item => item.id !== id);
 
 		localforage.setItem('wodList', newState)
@@ -64,16 +64,16 @@ class WodListContainer extends React.Component {
 			this.updateState(newState);
 		})
 		.catch(function (err) {
-		  console.error(err);
+			console.error(err);
 		});
-  }
+	}
 
 	render() {
 		return (
 			<MuiThemeProvider>
 				<div className="wod-list">
-          <AddWodForm addWod={this._adddWod} />
-          <Wodlist
+					<AddWodForm addWod={this._adddWod} />
+					<Wodlist
 						list={this.state.list}
 						deleteWod={this._deleteWod} />
 				</div>
